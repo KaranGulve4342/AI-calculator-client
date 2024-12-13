a66beb1a553062193c13457ef1e1b2f850aed33f
KaranGulve4342 <karangulve4342@gmail.com>
2024-12-13 22:31:35 +0530

    All files added

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import eslint from 'vite-plugin-eslint';
+// import { defineConfig } from "vite"
+ 
+// export default defineConfig({
+//   plugins: [react(),eslint()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
+import path from "path"
+import react from "@vitejs/plugin-react"
+import eslint  from '@rollup/plugin-eslint';
+import { defineConfig } from "vite"
+ 
+export default defineConfig({
+  plugins: [
+    react(),
+    eslint({ include: 'src/**/*.+(js|jsx|ts|tsx)' })
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
