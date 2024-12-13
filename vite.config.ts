633c4bf4662f14e5b0354b47c96024ebdd0dc135
KaranGulve4342 <karangulve4342@gmail.com>
2024-12-13 23:23:24 +0530

    added

--- vite.config.ts
+++ vite.config.ts
@@ -28,4 +28,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://ai-calculator-server-taupe.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
